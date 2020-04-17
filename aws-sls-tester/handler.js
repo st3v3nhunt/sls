438ed91a288f6e9e43f8137b4de321d91833069e
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2020-04-17 16:54:04 +0100

    Send message to queue when http endpoint is triggered

--- aws-sls-tester/handler.js
+++ aws-sls-tester/handler.js
@@ -1,6 +1,19 @@
-module.exports.echo = async event => {
+const sqs = require('aws-sdk/clients/sqs');
+
+module.exports.echo = async (event, ctx) => {
+  const region = ctx.invokedFunctionArn.split(':')[3];
+  const accountId = ctx.invokedFunctionArn.split(':')[4];
+  const queueName = 'MyQueue'; // comes from serverless.yml config
+  const queueUrl = `https://sqs.${region}.amazonaws.com/${accountId}/${queueName}`;
+
   return {
     statusCode: 200,
+    await sqs.sendMessage({
+      MessageBody: "MessageBody",
+      QueueUrl: queueUrl,
+    }).promise();
+    console.log(`Message sent to queue: ${queueUrl}`);
+
     body: JSON.stringify(
       {
         message: 'Hello cruel world!!',
