a566d3b7f46ee789cca243bc8aaedd65b78bf6e0
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2020-04-17 17:02:08 +0100

    Instantiate new SQS service

--- aws-sls-tester/handler.js
+++ aws-sls-tester/handler.js
@@ -1,19 +1,21 @@
-const sqs = require('aws-sdk/clients/sqs');
+const SQS = require('aws-sdk/clients/sqs');
 
 module.exports.echo = async (event, ctx) => {
   const region = ctx.invokedFunctionArn.split(':')[3];
   const accountId = ctx.invokedFunctionArn.split(':')[4];
   const queueName = 'MyQueue'; // comes from serverless.yml config
   const queueUrl = `https://sqs.${region}.amazonaws.com/${accountId}/${queueName}`;
+  // const sqs = new SQS();
+
+  await new SQS().sendMessage({
+    MessageBody: "MessageBody",
+    QueueUrl: queueUrl,
+  }).promise();
+  console.log(`Message sent to queue: ${queueUrl}`);
+  // console.log(sqs);
 
   return {
     statusCode: 200,
-    await sqs.sendMessage({
-      MessageBody: "MessageBody",
-      QueueUrl: queueUrl,
-    }).promise();
-    console.log(`Message sent to queue: ${queueUrl}`);
-
     body: JSON.stringify(
       {
         message: 'Hello cruel world!!',
