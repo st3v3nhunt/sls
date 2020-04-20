b2abaad1144bee8d588b942b181cb2057a12b631
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2020-04-20 10:53:17 +0100

    Install standard

--- aws-sls-tester/handler.js
+++ aws-sls-tester/handler.js
@@ -1,47 +1,47 @@
-const SQS = require('aws-sdk/clients/sqs');
+const SQS = require('aws-sdk/clients/sqs')
 
 module.exports.echo = async (event, ctx) => {
-  const body = event.body;
+  const body = event.body
   if (!body) {
     return {
       statusCode: 400,
       body: JSON.stringify({
         message: 'nah, nah, nah!'
-      }),
+      })
     }
   }
 
-  console.log('*****************ctx:');
-  console.log(ctx);
+  console.log('*****************ctx:')
+  console.log(ctx)
 
-  const region = ctx.invokedFunctionArn.split(':')[3];
-  const accountId = ctx.invokedFunctionArn.split(':')[4];
-  const queueName = 'MyQueue'; // comes from serverless.yml config
-  const queueUrl = `https://sqs.${region}.amazonaws.com/${accountId}/${queueName}`;
-  console.log(`queueUrl: ${queueUrl}`);
+  const region = ctx.invokedFunctionArn.split(':')[3]
+  const accountId = ctx.invokedFunctionArn.split(':')[4]
+  const queueName = 'MyQueue' // comes from serverless.yml config
+  const queueUrl = `https://sqs.${region}.amazonaws.com/${accountId}/${queueName}`
+  console.log(`queueUrl: ${queueUrl}`)
 
   await new SQS().sendMessage({
     MessageBody: body,
-    QueueUrl: queueUrl,
-  }).promise();
-  console.log(`Message sent to queue: ${queueUrl}`);
+    QueueUrl: queueUrl
+  }).promise()
+  console.log(`Message sent to queue: ${queueUrl}`)
 
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
         message: 'Hello cruel world!!',
-        input: event,
+        input: event
       },
       null,
       2
-    ),
-  };
-};
+    )
+  }
+}
 
 module.exports.processQ = async event => {
-  console.log(`Number of records in message: ${event.Records.length}`);
+  console.log(`Number of records in message: ${event.Records.length}`)
   event.Records.forEach((record, idx) => {
-    console.log(`Record (${idx}): `, record);
-  });
-};
+    console.log(`Record (${idx}): `, record)
+  })
+}
