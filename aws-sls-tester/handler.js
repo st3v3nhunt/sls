29061522de319af66b4e4436de2c70b7b6dfe93a
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2020-04-23 13:57:26 +0100

    Simplify and use body.message

--- aws-sls-tester/handler.js
+++ aws-sls-tester/handler.js
@@ -4,11 +4,11 @@ const s3 = new AWS.S3()
 const sqs = new AWS.SQS()
 
 module.exports.submit = async (event, ctx) => {
-  const imageUrl = event.imageUrl
-  if (!imageUrl) {
+  const body = event.body
+  if (!body && !body.message) {
     return {
       statusCode: 400,
-      body: JSON.stringify({ message: 'Please supply an imageUrl.' })
+      body: JSON.stringify({ message: 'Please supply a message in the body.' })
     }
   }
 
@@ -18,25 +18,24 @@ module.exports.submit = async (event, ctx) => {
   const queueUrl = `https://sqs.${region}.amazonaws.com/${accountId}/${queueName}`
 
   await sqs.sendMessage({
-    MessageBody: JSON.stringify({ imageUrl }),
+    MessageBody: JSON.stringify({ messge: body.message }),
     QueueUrl: queueUrl
   }).promise()
 
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: `${imageUrl} is being procesed.` })
+    body: JSON.stringify({ message: `${body.message} is being procesed.` })
   }
 }
 
 module.exports.process = async event => {
-  const buf = Buffer.from('some string')
   event.Records.forEach(async (record, idx) => {
-    console.log(record)
-    console.log('Processing message.')
-    s3.putObject({
+    console.log('Message body:')
+    console.log(record.body)
+    await s3.putObject({
       Bucket: process.env.BUCKET,
       Key: 'temp.txt',
-      Body: buf
+      Body: record.body.message
     }).promise()
     console.log('File has been created in s3')
   })
