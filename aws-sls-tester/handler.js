fcd32756526748175aa87e04af22a30434ae434d
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2020-04-23 14:17:02 +0100

    Add viewer function

--- aws-sls-tester/handler.js
+++ aws-sls-tester/handler.js
@@ -5,7 +5,7 @@ const sqs = new AWS.SQS()
 
 module.exports.submit = async (event, ctx) => {
   const body = event.body
-  if (!body && !body.message) {
+  if (!body || !body.message) {
     return {
       statusCode: 400,
       body: JSON.stringify({ message: 'Please supply a message in the body.' })
@@ -24,7 +24,7 @@ module.exports.submit = async (event, ctx) => {
 
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: `${body.message} is being procesed.` })
+    body: JSON.stringify({ message: `${body.message} is being processed.` })
   }
 }
 
@@ -39,4 +39,15 @@ module.exports.process = async event => {
     }).promise()
     console.log('File has been created in s3')
   })
+}
+
+module.exports.view = async event => {
+  const buf = await s3.getObject({
+    Bucket: process.env.BUCKET,
+    Key: 'temp.txt'
+  })
+  return {
+    statusCode: 200,
+    body: buf.toString()
+  }
 }
