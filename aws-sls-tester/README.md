41dc6593bbb2a8db0921bdc2265f92dd4670b033
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2020-04-23 12:59:34 +0100

    Add some file processing

--- aws-sls-tester/README.md
+++ aws-sls-tester/README.md
@@ -1,5 +1,11 @@
 # Severless testing
 
-> Create an http endpoint that will create a message, place it on a queue that
-  will in turn be processed. The http endpoint will respond with the details of
-  the event that called it.
+> The 'application' consists of an http endpoint that will create a message on
+  a queue based on the input data. Another function will take the body of the
+  message and update a file in an S3 bucket that has another function that will
+  load it to view.
+
+## Endpoints
+
+- `submit` - creates message from input
+- `process` - updates the S3 file
--- aws-sls-tester/handler.js
+++ aws-sls-tester/handler.js
@@ -1,47 +1,41 @@
-const SQS = require('aws-sdk/clients/sqs')
+const AWS = require('aws-sdk')
+const fs = require('fs').promises
 
-module.exports.echo = async (event, ctx) => {
-  const body = event.body
-  if (!body) {
+module.exports.submit = async (event, ctx) => {
+  const imageUrl = event.imageUrl
+  if (!imageUrl) {
     return {
       statusCode: 400,
-      body: JSON.stringify({
-        message: 'nah, nah, nah!'
-      })
+      body: JSON.stringify({ message: 'Please supply an imageUrl.' })
     }
   }
-
-  console.log('*****************ctx:')
-  console.log(ctx)
 
   const region = ctx.invokedFunctionArn.split(':')[3]
   const accountId = ctx.invokedFunctionArn.split(':')[4]
   const queueName = process.env.QUEUE_NAME
   const queueUrl = `https://sqs.${region}.amazonaws.com/${accountId}/${queueName}`
-  console.log(`queueUrl: ${queueUrl}`)
 
-  await new SQS().sendMessage({
-    MessageBody: body,
+  await new AWS.SQS().sendMessage({
+    MessageBody: { imageUrl },
     QueueUrl: queueUrl
   }).promise()
-  console.log(`Message sent to queue: ${queueUrl}`)
 
   return {
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Hello cruel world!!',
-        input: event
-      },
-      null,
-      2
-    )
+    body: JSON.stringify({ message: `${imageUrl} is being procesed.` })
   }
 }
 
-module.exports.processQ = async event => {
-  console.log(`Number of records in message: ${event.Records.length}`)
-  event.Records.forEach((record, idx) => {
-    console.log(`Record (${idx}): `, record)
+module.exports.process = async event => {
+  const s3 = await new AWS.S3()
+  event.Records.forEach(async (record, idx) => {
+    console.log(record)
+    await fs.writeFile('temp.txt', 'record.message', 'utf8')
+    console.log('Processing message.')
+    s3.putObject({
+      Bucket: process.env.BUCKET,
+      Key: 'temp.txt',
+      Body: await fs.readFile('temp.txt')
+    })
   })
 }
