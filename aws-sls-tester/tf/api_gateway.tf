f12743cf2f7062685b09d9ad0945790ac9171506
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2020-04-24 18:26:32 +0100

    Use AWS_PROXY for as-is pass through

--- aws-sls-tester/tf/api_gateway.tf
+++ aws-sls-tester/tf/api_gateway.tf
@@ -6,7 +6,7 @@ resource "aws_api_gateway_rest_api" "view" {
 resource "aws_api_gateway_resource" "view" {
   rest_api_id = aws_api_gateway_rest_api.view.id
   parent_id   = aws_api_gateway_rest_api.view.root_resource_id
-  path_part   = "hello"
+  path_part   = "view"
 }
 
 resource "aws_api_gateway_method" "view" {
@@ -21,14 +21,13 @@ resource "aws_api_gateway_integration" "view" {
   http_method = aws_api_gateway_method.view.http_method
 
   integration_http_method = "POST"
-  type                    = "AWS"
+  type                    = "AWS_PROXY"
   uri                     = aws_lambda_function.view.invoke_arn
 }
 
 resource "aws_api_gateway_deployment" "view" {
   depends_on = [
     aws_api_gateway_integration.view
-    # aws_api_gateway_integration.lambda_root,
   ]
 
   rest_api_id = aws_api_gateway_rest_api.view.id
@@ -47,13 +46,13 @@ resource "aws_api_gateway_rest_api" "submit" {
 resource "aws_api_gateway_resource" "submit" {
   rest_api_id = aws_api_gateway_rest_api.submit.id
   parent_id   = aws_api_gateway_rest_api.submit.root_resource_id
-  path_part   = "hello"
+  path_part   = "submit"
 }
 
 resource "aws_api_gateway_method" "submit" {
   rest_api_id   = aws_api_gateway_rest_api.submit.id
   resource_id   = aws_api_gateway_resource.submit.id
-  http_method   = "GET"
+  http_method   = "POST"
   authorization = "NONE"
 }
 resource "aws_api_gateway_integration" "submit" {
@@ -62,7 +61,7 @@ resource "aws_api_gateway_integration" "submit" {
   http_method = aws_api_gateway_method.submit.http_method
 
   integration_http_method = "POST"
-  type                    = "AWS"
+  type                    = "AWS_PROXY"
   uri                     = aws_lambda_function.submit.invoke_arn
 }
 
@@ -88,7 +87,7 @@ output "base_url-submit" {
 # resource "aws_api_gateway_resource" "process" {
 #   rest_api_id = aws_api_gateway_rest_api.process.id
 #   parent_id   = aws_api_gateway_rest_api.process.root_resource_id
-#   path_part   = "hello"
+#   path_part   = "process"
 # }
 
 # resource "aws_api_gateway_method" "process" {
